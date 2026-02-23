10877e95f5a79fd20f6e39eeab3832cba485a86e
Mattojjo <leinad9607@gmail.com>
2026-02-23 17:06:28 -0500

    refactor: streamline blog post loading and enhance dynamic rendering

--- scripts/blogPosts.js
+++ scripts/blogPosts.js
@@ -1,19 +1,41 @@
 async function blogPosts() {
-    const postsContainer = document.getElementById('posts');
+    let posts = [];
 
-        try {
-            const response = await fetch('./posts.json');
-            const data = await response.json();
-            
-            data.posts.forEach(post => {
-                const postElement = document.createElement('div');
-                postElement.className = 'post';
-                postElement.innerHTML = `<h2>${post.title}</h2><p>${post.description}</p>`;
-                postsContainer.appendChild(postElement);
-    });
-        } catch (e) {
-            console.log("Error", e);
-        }
+    const postsContainer = document.getElementById('posts');
+    const nextButton = document.getElementById('nextButton');
+    let start = 0;
+    let end = start += 10;
+
+    try {
+        const response = await fetch('./posts.json');
+        const data = await response.json();
+        posts = Array.isArray(data.posts) ? data.posts : data;
+        load10(posts)
+
+    } catch (error) {
+        console.log("Error", error);
     }
+
+    function load10(posts) {
+        const initialArray = posts.slice(start, start += end);
+        initialArray.forEach(post => {
+            const postElement = document.createElement('div');
+            postElement.className = 'post';
+            postElement.innerHTML = `<h2>${post.title}</h2><p>${post.description}</p>`
+            postsContainer.appendChild(postElement);
+        })
+    };
+
+    function nextPosts(start, end) {
+        let newEnd = end += 10;
+        console.log(start, newEnd)
+        const nextArray = posts.slice(start, start += newEnd)
+
+        console.log(nextArray);
+
+    }
+ 
+  nextPosts()
+}
 
 window.onload = blogPosts;  
