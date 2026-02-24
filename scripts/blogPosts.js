4a6d749579a68d87dd2a71c375086d346fefa3fc
Mattojjo <leinad9607@gmail.com>
2026-02-24 08:04:32 -0500

    refactor: reorganize HTML structure and enhance blog post navigation

--- scripts/blogPosts.js
+++ scripts/blogPosts.js
@@ -1,41 +1,92 @@
-async function blogPosts() {
-    let posts = [];
+let postsContainer = document.createElement('div')
+let previousButton = document.createElement('button')
+let nextButton = document.createElement('button')
+let posts = [];
+let currentArray = []
 
-    const postsContainer = document.getElementById('posts');
-    const nextButton = document.getElementById('nextButton');
-    let start = 0;
-    let end = start += 10;
+const root = document.getElementById('root');
+let start = 0;
+const end = 10;
 
+function init() {
+    postsContainer.id = 'posts';
+    nextButton.id = 'nextButton';
+    previousButton.id = 'preciousButton';
+    nextButton.className = 'postButtons';
+    nextButton.innerHTML = 'Next';
+    previousButton.className = 'postButtons';
+    previousButton.innerHTML = 'Previous';
+
+    root.appendChild(postsContainer);
+    root.appendChild(previousButton);
+    root.appendChild(nextButton);
+
+}
+
+init()
+
+async function fetchData() {
     try {
         const response = await fetch('./posts.json');
         const data = await response.json();
-        posts = Array.isArray(data.posts) ? data.posts : data;
-        load10(posts)
-
+        posts = data.posts;
     } catch (error) {
         console.log("Error", error);
     }
-
-    function load10(posts) {
-        const initialArray = posts.slice(start, start += end);
-        initialArray.forEach(post => {
-            const postElement = document.createElement('div');
-            postElement.className = 'post';
-            postElement.innerHTML = `<h2>${post.title}</h2><p>${post.description}</p>`
-            postsContainer.appendChild(postElement);
-        })
-    };
-
-    function nextPosts(start, end) {
-        let newEnd = end += 10;
-        console.log(start, newEnd)
-        const nextArray = posts.slice(start, start += newEnd)
-
-        console.log(nextArray);
-
-    }
- 
-  nextPosts()
 }
 
-window.onload = blogPosts;  
+function arraySlice(arg) {     
+    if (arg === 'previous'){
+        start = Math.max(0, start - end);
+        return currentArray = posts.slice(start, start + end);
+    } else if (arg === 'next') {
+        start += end;
+        return currentArray = posts.slice(start - end, start);
+    } else {
+        currentArray = posts.slice(start, start + end);
+        start = 0
+        previousButton.disabled = true;
+    }
+}
+
+function load10(arg) {
+    arraySlice(arg);
+    currentArray.forEach(post => {
+        const postElement = document.createElement('div');
+        postElement.className = 'post';
+        postElement.innerHTML = `<h2>${post.title}</h2><p>${post.description}</p>`
+        postsContainer.appendChild(postElement);
+    })
+    console.log(currentArray, start, end)
+};
+
+function nextPosts() {
+    let next = 'next';
+    if (Array.isArray(posts) && posts.length) {
+        postsContainer.innerHTML = '';
+        load10(next);
+
+        nextButton.disabled = start >= posts.length;
+        previousButton.disabled = start <= 0;
+    }
+};
+
+function previousPosts() {
+    let previous = 'previous';
+    if (Array.isArray(posts) && posts.length) {
+        postsContainer.innerHTML = '';
+        load10(previous);
+
+        nextButton.disabled = start >= posts.length;
+        previousButton.disabled = start <= 0;
+    }
+
+};
+
+nextButton.addEventListener('click', nextPosts);
+previousButton.addEventListener('click', previousPosts)
+
+window.onload = async () => {
+    await fetchData();
+    load10();
+};
